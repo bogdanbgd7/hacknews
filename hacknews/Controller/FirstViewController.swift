5b949df511ceff9bc82ee3466c79b71d8f961512
bogdanbgd7 <bogdan.ponocko@gmail.com>
2020-10-23 16:45:53 +0200

    first commit

---
+++ hacknews/Controller/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  hacknews
+//
+//  Created by Bogdan Ponocko on 23/10/2020.
+//  Copyright © 2020 Bogdan Ponocko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hacknews/Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  hacknews
+//
+//  Created by Bogdan Ponocko on 23/10/2020.
+//  Copyright © 2020 Bogdan Ponocko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
