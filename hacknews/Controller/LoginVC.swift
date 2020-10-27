5285aa501b9c0ea3658bcbf0db62f522234d4cfc
bogdanbgd7 <bogdan.ponocko@gmail.com>
2020-10-27 13:08:01 +0100

    added LoginVC and AuthVC + new placeholder attributes

---
+++ hacknews/Controller/LoginVC.swift
@@ -0,0 +1,43 @@
+//
+//  LoginVC.swift
+//  hacknews
+//
+//  Created by Bogdan Ponocko on 27/10/2020.
+//  Copyright © 2020 Bogdan Ponocko. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension UITextField {
+    @IBInspectable var placeholderColor: UIColor {
+        get {
+            return attributedPlaceholder?.attribute(.foregroundColor, at: 0, effectiveRange: nil) as? UIColor ?? .clear
+        }
+        set {
+            guard let attributedPlaceholder = attributedPlaceholder else { return }
+            let attributes: [NSAttributedString.Key: UIColor] = [.foregroundColor: newValue]
+            self.attributedPlaceholder = NSAttributedString(string: attributedPlaceholder.string, attributes: attributes)
+        }
+    }
+}
