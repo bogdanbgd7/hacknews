2d456c449e66e6433e3b0345e45c37ab0a56f8bc
bogdanbgd7 <bogdan.ponocko@gmail.com>
2020-10-27 13:47:10 +0100

    added AuthService

---
+++ hacknews/Services/AuthService.swift
@@ -0,0 +1,51 @@
+//
+//  AuthService.swift
+//  hacknews
+//
+//  Created by Bogdan Ponocko on 27/10/2020.
+//  Copyright © 2020 Bogdan Ponocko. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            guard let user = authResult?.user else {
+                userCreationComplete(false, error)
+                return
+            }
+            let userData = ["provider": user.providerID, "email": user.email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+        
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+        
+            //unsuccessful
+            guard let user = authResult?.user else {
+                loginComplete(false, error)
+                return
+            }
+            
+            //successfully authenticated
+            loginComplete(true, nil)
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+}
