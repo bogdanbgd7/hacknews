05112838e523a949dd4c129c2977939cf83a7445
bogdanbgd7 <bogdan.ponocko@gmail.com>
2020-11-04 18:28:43 +0100

    created ProfileVC

---
+++ hacknews/Controller/ProfileVC.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileVC.swift
+//  hacknews
+//
+//  Created by Bogdan Ponocko on 04/11/2020.
+//  Copyright © 2020 Bogdan Ponocko. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
