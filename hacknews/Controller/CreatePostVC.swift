a36647c0a697a49dd9b6c58795c2346906660bfd
bogdanbgd7 <bogdan.ponocko@gmail.com>
2020-11-09 16:35:00 +0100

    CreatePostVC created with init actions and outlets

---
+++ hacknews/Controller/CreatePostVC.swift
@@ -0,0 +1,30 @@
+//
+//  CreatePostVC.swift
+//  hacknews
+//
+//  Created by Bogdan Ponocko on 09/11/2020.
+//  Copyright © 2020 Bogdan Ponocko. All rights reserved.
+//
+
+import UIKit
+
+class CreatePostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
